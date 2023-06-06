d7783d25cdcdd697a6b2bec35fcbd4f117c10ae5
ElmaHarmsen <elmaharmsen01@gmail.com>
2023-06-06 15:48:27 +0200

    Added global styling
    Font
    Styling of headings and text
    Dot and chevron

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="section__top">
+      <h1 class="heading__special">devoted human computer interaction designer</h1>
+      <img src="../assets/cloud.png" class="cloud" alt="white cloud with blue shadow" />
+      <span class="dot dot__active"></span>
+      <span class="chevron chevron__down"></span>
+    </div>
+    <div class="section__bottom">
+      <h2>header</h2>
+      <p>body text here <br />more body text</p>
+    </div>
   </main>
 </template>
+
+<style>
+.section__top,
+.section__bottom {
+  min-height: 100vh;
+  padding: 1rem;
+}
+.section__bottom {
+  background-color: var(--white);
+}
+</style>
